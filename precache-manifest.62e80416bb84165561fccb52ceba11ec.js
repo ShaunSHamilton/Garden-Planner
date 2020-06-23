a59134844a66bdc78b51af8078184e92927db82d
Sky020 <2397860h@student.gla.ac.uk>
2020-06-23 20:17:44 +0100

    Updates

---
+++ precache-manifest.62e80416bb84165561fccb52ceba11ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7372e793df27fa5dcec646ff48b1d916",
+    "url": "/Garden-Planner/index.html"
+  },
+  {
+    "revision": "3cbbcd04e1ccd726cb4d",
+    "url": "/Garden-Planner/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "58fc4dfa908a44a9669d",
+    "url": "/Garden-Planner/static/css/main.c1c97bdc.chunk.css"
+  },
+  {
+    "revision": "3cbbcd04e1ccd726cb4d",
+    "url": "/Garden-Planner/static/js/2.0d4f771e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Garden-Planner/static/js/2.0d4f771e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58fc4dfa908a44a9669d",
+    "url": "/Garden-Planner/static/js/main.66bf2b67.chunk.js"
+  },
+  {
+    "revision": "b04a185c9a63f442150c",
+    "url": "/Garden-Planner/static/js/runtime-main.5d4f209d.js"
+  },
+  {
+    "revision": "b50d45c29f0a4a97bc4fed5ea4ffe2a7",
+    "url": "/Garden-Planner/static/media/english-111-vivace.b50d45c2.ttf"
+  }
+]);
